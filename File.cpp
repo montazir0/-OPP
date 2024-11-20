6ad80f44a8c42e16fb78a64b387cbc3c2e4d3d5c
Montazir Nasser <115963375+montazir0@users.noreply.github.com>
2024-11-20 22:39:50 +0300

    Add files via upload

---
+++ File.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+#include "clsMainScreen.h"
+
+int main()
+{
+    clsMainScreen::ShowMainMenue();
+
+    system("pause>0");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
